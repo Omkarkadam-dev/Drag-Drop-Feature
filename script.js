9f95b49826523c656ec0069c2ad927c5626e17ac
Omkar Kadam <omkarkadam1115@gmail.com>
2024-10-31 23:44:03 +0530

    add js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("hello")
+let lists = document.getElementsByClassName("list");
+
+let RightBox = document.getElementById("right");
+let LeftBox = document.getElementById("left");
+
+for (list of lists) {
+    list.addEventListener("dragstart", function (e) {
+        let selected = e.target;
+
+        RightBox.addEventListener("dragover", function (e) {
+            e.preventDefault();
+        });
+
+        RightBox.addEventListener("drop", function (e) {
+            RightBox.appendChild(selected);
+            selected = null;
+        });
+
+
+
+        LeftBox.addEventListener("dragover", function (e) {
+            e.preventDefault();
+        });
+
+        LeftBox.addEventListener("drop", function (e) {
+            LeftBox.appendChild(selected);
+            selected = null;
+        });
+    })
+}
